5ad60c9baeaf12a9c00c8d2a880ab575c6877766
TayGov <taylor.clausen@gov.bc.ca>
2021-02-18 06:59:11 -0800

    Refactoring

--- src/main/java/ca/bc/gov/open/efilingdiligenclient/diligen/DiligenService.java
+++
@@ -1,9 +0,0 @@
-package ca.bc.gov.open.efilingdiligenclient.diligen;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.math.BigDecimal;
-
-public interface DiligenService {
-    BigDecimal postDocument(String documentType, MultipartFile file);
-}
