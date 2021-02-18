45dd32de594ef03ae72f98ea6c2325da5d3b4521
TayGov <taylor.clausen@gov.bc.ca>
2021-02-18 09:27:55 -0800

    Refactor

--- src/main/java/ca/bc/gov/open/jag/efilingreviewerapi/extract/DiligenService.java
+++
@@ -1,9 +0,0 @@
-package ca.bc.gov.open.jag.efilingreviewerapi.extract;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.math.BigDecimal;
-
-public interface DiligenService {
-    BigDecimal postDocument(String documentType, MultipartFile file);
-}
