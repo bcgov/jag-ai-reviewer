889aab545bf4c96cf378061fef304233fe8f7ce6
Celina <yousiefc@pm.me>
2021-05-12 23:00:34 -0700

    env vars for auth

--- src/frontend/ai-reviewer-admin/src/EnvConfig.js
+++ src/frontend/ai-reviewer-admin/src/EnvConfig.js
@@ -1,3 +1,15 @@
 export const REACT_APP_AI_REVIEWER_API_URL = window.env
   ? window.env.REACT_APP_AI_REVIEWER_API_URL
   : process.env.REACT_APP_AI_REVIEWER_API_URL;
+
+export const url = window.env
+  ? window.env.REACT_APP_KEYCLOAK_URL
+  : process.env.REACT_APP_KEYCLOAK_URL;
+
+export const realm = window.env
+  ? window.env.REACT_APP_KEYCLOAK_REALM
+  : process.env.REACT_APP_KEYCLOAK_REALM;
+  
+export const clientId = window.env
+  ? window.env.REACT_APP_KEYCLOAK_CLIENT_ID
+  : process.env.REACT_APP_KEYCLOAK_CLIENT_ID;
