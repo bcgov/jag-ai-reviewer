6a1580cecddbcc5935ef801a8bb89ac55c970581
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2021-05-17 13:29:29 -0700

    Renamed EfilingReviewerApiApplication to AIReviewerApiApplication

--- src/backend/ai-reviewer-api/src/main/java/ca/bc/gov/open/jag/aireviewerapi/AIReviewerApiApplication.java
+++ src/backend/ai-reviewer-api/src/main/java/ca/bc/gov/open/jag/aireviewerapi/AIReviewerApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EfilingReviewerApiApplication {
+public class AIReviewerApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EfilingReviewerApiApplication.class, args);
+		SpringApplication.run(AIReviewerApiApplication.class, args);
 	}
 
 }
