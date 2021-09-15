0198eb8c1825528e2024dc53882ca00cea084863
TayGov <taylor.clausen@gov.bc.ca>
2021-09-15 07:33:20 -0700

    Remove unnecessary annotation

--- src/backend/ai-reviewer-mock-api/src/test/java/ca/bc/gov/open/jag/aireviewermockapi/controller/ParentControllerTest.java
+++ src/backend/ai-reviewer-mock-api/src/test/java/ca/bc/gov/open/jag/aireviewermockapi/controller/ParentControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ParentControllerTest {
 
     private static final String DOCUMENT_TYPE = "TYPE";
