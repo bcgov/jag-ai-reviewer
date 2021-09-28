ee5ee9f96c9d0699f0036d9fcc6a82e9e54786da
TayGov <taylor.clausen@gov.bc.ca>
2021-09-28 08:17:27 -0700

    Adding auto file and some refactoring

---
+++ src/backend/ai-reviewer-api/src/main/java/ca/bc/gov/open/jag/aireviewerapi/cso/model/CSOResult.java
@@ -0,0 +1,22 @@
+package ca.bc.gov.open.jag.aireviewerapi.cso.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+
+public class CSOResult {
+
+    @JsonProperty("success")
+    private Boolean success;
+    @JsonProperty("packageId")
+    private BigDecimal packageId;
+
+    public Boolean getSuccess() {  return success; }
+
+    public void setSuccess(Boolean success) { this.success = success; }
+
+    public BigDecimal getPackageId() { return packageId;  }
+
+    public void setPackageId(BigDecimal packageId) { this.packageId = packageId; }
+
+}
